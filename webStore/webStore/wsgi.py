b37ec1dc31a56a63236edee0d164f53f30449bcf
Unknown <victor@pistache.co>
2018-09-05 19:31:20 -0300

    Start use Cling

--- webStore/webStore/wsgi.py
+++ webStore/webStore/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webStore.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webStore.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
