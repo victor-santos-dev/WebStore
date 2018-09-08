edacd7f114643ab1325d13fb63047168c2a3da1b
Victor Santos <victor@pistache.co>
2018-09-07 22:36:27 -0300

    Remove api/root url

--- webStore/urls.py
+++ webStore/urls.py
@@ -15,25 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
 import catalog.urls
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
     path('api/',include(catalog.urls)),
-    path('api/root/',include(router.urls)),
     path('admin/', admin.site.urls),
 ]
